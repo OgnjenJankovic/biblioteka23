88e3a67d670603afd1dc1388d190352862e65e1b
2000o <2000o@DESKTOP-S1RMM04>
2023-04-09 18:38:26 +0200

    Dodali smo klasu Knjiga

---
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -0,0 +1,39 @@
+package biblioteka;
+
+public class Knjiga {
+
+	String naslov;
+	int isbn;
+	String autori;
+	String izdavac;
+	int izdanje;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
